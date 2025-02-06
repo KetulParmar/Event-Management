72377c89875a54a91009cb0377f93b7febda7f1e
Ketul Parmar <ketul4947@gmail.com>
2025-02-06 17:49:25 +0530

    3rd

--- Event/accounts/models.py
+++ Event/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class user_Data(models.Model):
+    USER_TYPE_CHOICES = [
+        ('attendee', 'Attendee'),
+        ('organizer', 'Organizer'),
+    ]
+    objects = None
+    Username = models.CharField(max_length=50,default=None, unique=True)
+    Name = models.CharField(max_length=50)
+    Email = models.EmailField()
+    Password = models.CharField(max_length=500)
+    Phone = models.CharField(max_length=10)
+    User_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='attendee')
+    #additonal for Organizer
+    Company_Name = models.CharField(max_length=20, blank=True, null=True)
+    Website = models.CharField(max_length=255, blank=True, null=True)
