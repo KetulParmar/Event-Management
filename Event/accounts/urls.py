7a9ed73f9f828fa705e0755fb27f25a5d453ca70
Ketul Parmar <ketul4947@gmail.com>
2025-02-07 22:47:27 +0530

    login and registration done

--- Event/accounts/urls.py
+++ Event/accounts/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('Login', views.login),
-    path('Register',views.register)
+    path('Register',views.register),
+    path('Organizer',views.organizer),
+    path('Attendee',views.attendee),
+    path('logout/', views.logout_view, name='logout'),
 ]
